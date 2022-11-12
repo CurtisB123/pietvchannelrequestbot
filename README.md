0a1b53ae2a697f5edc420cde18a5b7fb0a7468e8
CurtisB123 <117991968+CurtisB123@users.noreply.github.com>
2022-11-12 22:24:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Telegram-Request-Bot
+A simple telegram bot for requesting features related to your project from users.
+
+#### Requirements
+The project uses python-telegram-bot and sqlite3
---
+++ config.py
@@ -0,0 +1,4 @@
+#Contains configuration details
+bot_token = 5709909986:AAE5Yf-35JMJmsfCv_J-oa7j-MADc3p-q0Y
+bot_user_name = PieTVChannelRequestbot
+URL = "the heroku app link that we will create later"
